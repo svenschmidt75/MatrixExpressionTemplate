4de57e4eef544e2323d0b537e8119e9033280d42
unknown <Sven@Sven-PC.(none)>
2011-11-13 10:42:50 -0600

    Added type_traits file

---
+++ MatrixExpressionTemplate/details/type_traits.h
@@ -0,0 +1,37 @@
+/*
+ * Name  : type_traits
+ * Path  : 
+ * Use   : Abstraction for types. Advantage is that we only need to change type_traits, not where it is used.
+ *         If no type_traits is used, we would have to change all occurrences individually.
+ * Author: Sven Schmidt
+ * Date  : 11/13/2011
+ */
+#pragma once
+
+// Forward declaration of used types
+template<typename T, int ROWS, int COLS>
+class CMatrix;
+
+template<typename OP1, typename OP2, typename BINARY_OPERATOR>
+class CMatrixBinaryExpression;
+
+
+// Base type traits
+template<typename ComplexType>
+struct type_traits {
+    typedef typename ComplexType::value_type value_type;
+};
+
+// Specialization for CMatrix
+template<typename T, int ROWS, int COLS>
+struct type_traits<CMatrix<T, ROWS, COLS>> {
+    typedef T                      value_type;
+    typedef CMatrix<T, ROWS, COLS> RefType;
+};
+
+// Specialization for CMatrixBinaryExpression
+template<typename OP1, typename OP2, typename BINARY_OPERATOR>
+struct type_traits<CMatrixBinaryExpression<OP1, OP2, BINARY_OPERATOR>> {
+    typedef typename type_traits<OP1>::value_type              value_type;
+    typedef CMatrixBinaryExpression<OP1, OP2, BINARY_OPERATOR> RefType;
+};
