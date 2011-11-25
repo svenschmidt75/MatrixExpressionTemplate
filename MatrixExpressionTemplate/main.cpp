1053293af419854c970dc6fb71e1f628cc6b12fa
unknown <Sven@Sven-PC.(none)>
2011-11-25 08:35:28 -0600

    Minor modifications

--- MatrixExpressionTemplate/main.cpp
+++ MatrixExpressionTemplate/main.cpp
@@ -4,25 +4,36 @@
 #include "CMatrix.hpp"
 
 int main() {
-    CMatrix<double, 3, 3> a;
-    CMatrix<double, 3, 3> b;
-    CMatrix<double, 3, 3> c;
+    {
+        CMatrix<double, 3, 3> a;
+        CMatrix<double, 3, 3> b;
+        CMatrix<double, 3, 3> c;
 
-    // Matrix addition
-//    a = b + c;
-    (a + (b + c))[1][2];
-    ((b + c) + a)[1][2];
-    ((b + c) + (a + b))[1][2];
+        // Matrix addition
+        //    a = b + c;
+        (a + (b + c))[1][2];
+        ((b + c) + a)[1][2];
+        ((b + c) + (a + b))[1][2];
+    }
 
-    (a - (b + c))[1][2];
-    ((b - c) + a)[1][2];
-    ((b - c) + (a - b))[1][2];
+    {
+        CMatrix<double, 3, 3> a;
+        CMatrix<double, 3, 3> b;
+        CMatrix<double, 3, 3> c;
 
-    (a * (b + c))[1][2];
-    ((b * c) + a)[1][2];
-    ((b + c) * (a + b))[1][2];
+        (a - (b + c))[1][2];
+        ((b - c) + a)[1][2];
+        ((b - c) + (a - b))[1][2];
+    }
 
-    const double erg = (b + c)[1][2];
+    {
+        CMatrix<double, 3, 4> a;
+        CMatrix<double, 4, 4> b;
+        CMatrix<double, 3, 4> c;
+
+        (a * b)[1][2];
+        (c + (a * b))[1][2];
+    }
 
     return 0;
 }
